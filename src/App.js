e575d1d46f4ea33c6452d31d519345f3b7f798e9
Brendt Bly <brendt.bly@outlook.com>
2018-01-01 15:43:01 -0600

    cat list shows up client side

--- src/App.js
+++ src/App.js
@@ -3,17 +3,39 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-
+constructor () {
+  super();
+  this.state = {
+    cats: [
+      {name: 'Jimmy',
+      skill: 'shredding couches'},
+      {
+        name: 'Birtha',
+        skill: 'running around at 3AM'
+      }
+    ]
+  }
+}
   render() {
+//    change key to cat.id
+    const catsList = this.state.cats.map((cat, i) => 
+    
+      <li key={i.toString()}>
+        Name: {cat.name} &nbsp
+        Skill: {cat.skill}
+      </li> 
+      
+    )
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-intro">
+          <ol>{catsList}</ol>
+        </div>
       </div>
     );
   }
