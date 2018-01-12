91b120afbbf93d8c424ba9da3bf7a97795264087
Brendt Bly <brendt.bly@outlook.com>
2018-01-12 13:16:01 -0600

    injury-info styling

--- pain-free-running/src/components/injury-info.js
+++ pain-free-running/src/components/injury-info.js
@@ -20,54 +20,79 @@ class InjuryInfo extends Component {
         console.log(this.props);
         return (
             <div>
-                <div className="card-container">
+                <div className="container">
+
+                    {/* TITLE */}
+
                     <div className="columns">
-                        <div className="column col-md-6">
+                        <div className="column col-6 col-mx-auto">
                             <div className="card">
 
                                 <div className="card-header">
                                     <div className="card-title-line">
                                         <Link to='/'><button className="btn btn-sm arrow-left" aria-label="back"><i className="icon icon-arrow-left"></i></button></Link>
                                     </div>
-                                        <div className="card-title-line">
-                                            <h4 className="card-title ">&nbsp; {this.props.injuryInfo.title}</h4>
-                                        </div>
+                                    <div className="card-title-line">
+                                        <h4 className="card-title ">&nbsp; {this.props.injuryInfo.title}</h4>
                                     </div>
-                                    {/* <Note
-          toggleEdit={this.toggleEdit}
-          updatePerson={this.handleUpdatePerson}
-          edit={this.state.editReason}
-          handleReasonChange={this.handleReasonChange}
-          content={this.state.reason} /> */}
-                                    <small className="">
-                                        {this.props.injuryInfo.description}
-                                    </small>
+                                </div>
+                                <small className="">
+                                    {this.props.injuryInfo.description}
+                                </small>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+                <div className="columns">
+                    <div className="column col-4"></div>
+                    <div className="column col-6 col-mr-auto treatment-title"><h4>Treatments: </h4></div>
+
+                </div>
+
+                {/* TREATMENTS LIST */}
+
+                <div className="columns">
+                    <div className="column col-6 col-mx-auto">
+                        <div className="card">
+
+                            <div className="card-header">
+                                <div className="card-title-line">
+                                    <Link to='/'><button className="btn btn-sm arrow-left" aria-label="back"><i className="icon icon-arrow-left"></i></button></Link>
+                                </div>
+                                <div className="card-title-line">
+                                    <h4 className="card-title ">&nbsp; {this.props.injuryInfo.title}</h4>
                                 </div>
                             </div>
+                            <small className="">
+                                {this.props.injuryInfo.description}
+                            </small>
                         </div>
                     </div>
 
                 </div>
-                );
+
+            </div>
+        );
     }
 }
 
 // InjuryList.propTypes = {
-                    //     injuryList: PropTypes.arrayOf(PropTypes.shape({ id: PropTypes.number, title: PropTypes.string, description: PropTypes.string }))
+//     injuryList: PropTypes.arrayOf(PropTypes.shape({ id: PropTypes.number, title: PropTypes.string, description: PropTypes.string }))
 
-                    // }
+// }
 
-                    function mapStateToProps(state) {
-                        console.log('InjuryList mapStateToProps called, state: ');
-                        console.log(state);
-                        return {
-                            injuryInfo: state.injuryInfo
-                        };
-                    }
+function mapStateToProps(state) {
+    console.log('InjuryList mapStateToProps called, state: ');
+    console.log(state);
+    return {
+        injuryInfo: state.injuryInfo
+    };
+}
 
 function mapDispatchToProps(dispatch) {
     return bindActionCreators({
-                    getInjuryInfo: getInjuryInfo
+        getInjuryInfo: getInjuryInfo
     }, dispatch);
 }
 
